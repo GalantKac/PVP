2368c5b667b6e2950c08624b1b0679731b78c728
kacper21galantowicz@gmail.com <kacper21galant@gmail.com>
2020-01-12 17:00:24 +0100

    Start menu

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Project.MenuManager
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [Header("Server List")]
+        [SerializeField]
+        GameObject serverLsit;
+        [SerializeField]
+        Scrollbar serverListScrollbar;
+
+        private void Start()
+        {
+            serverLsit.SetActive(false);
+        }
+
+        public void CreateServer()
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        public void ShowServerList()
+        {
+            serverLsit.SetActive(true);
+            serverListScrollbar.value = 1;
+        }
+
+        public void CloseServerList()
+        {
+            serverLsit.SetActive(false);
+        }
+
+        public void JoinToServer()
+        {
+            //TODO
+        }
+
+        public void Options()
+        {
+            SceneManager.LoadScene("Options");
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
