5d420b35f4749905f2efcbe4fb183a833de26e33
kacper21galantowicz@gmail.com <kacper21galant@gmail.com>
2020-01-12 18:05:48 +0100

    Login and register menu

---
+++ Assets/Scripts/Loign/LoginManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject warrning;
+    public void Start()
+    {
+        warrning.SetActive(false);  
+    }
+
+    public void Login()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Register()
+    {
+        SceneManager.LoadScene("Register");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Register/RegisterManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class RegisterManager : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI inputName;
+    [SerializeField]
+    TextMeshProUGUI inputPassword;
+    [SerializeField]
+    GameObject warriningMessage;
+
+    private void Start()
+    {
+        warriningMessage.SetActive(false);
+    }
+
+    public void CreateUser()
+    {
+        //TODO
+    }
+
+    public void BackToLogin()
+    {
+        SceneManager.LoadScene("Login");
+    }
+}
