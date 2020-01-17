e7bd4f81c0284b28825d64774a03251eb8aa827f
kacper21galantowicz@gmail.com <kacper21galant@gmail.com>
2020-01-17 19:15:23 +0100

    delete server

--- Server/return.js
+++
@@ -1,9 +0,0 @@
-const return_JSON = function(json_data, response) {
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "application/json");
-    response.end(JSON.stringify(json_data));
-};
-
-module.exports = {
-    json: return_JSON
-};
